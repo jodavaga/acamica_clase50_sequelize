56643303d5733c4d43b7612b9a4fc52c2da6d065
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-06-08 18:36:48 -0500

    connect to Db using sequelize and get bandas

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
 
-
+// Sequielize
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:@localhost:3306/clase49');
 
 const app = express();
 
 app.use(express.json());
 
 
+app.get('/bandas', (req, res) => {
+    const query = 'SELECT * FROM bandas';
+    
+    sequelize.query(query, {type: sequelize.QueryTypes.SELECT} )
+        .then((response) => {
+            res.json(response);
+        }).catch((e) => console.log(e));
+});
 
 
 app.listen(4000, (req, res) => console.log('Escuchando por el 4000'));
