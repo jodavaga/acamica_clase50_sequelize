09c4b58642de936e0346238cf49dcf245a2adb46
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-06-08 19:26:00 -0500

    nueva forma de crear endpont /bandas

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/bandas', (req, res) => {
-    const query = 'SELECT * FROM bandas';
+// app.get('/bandas', (req, res) => {
+//     const query = 'SELECT * FROM bandas';
     
-    sequelize.query(query, {type: sequelize.QueryTypes.SELECT} )
-        .then((response) => {
-            res.json(response);
-        }).catch((e) => console.log(e));
+//     sequelize.query(query, {type: sequelize.QueryTypes.SELECT} )
+//         .then((response) => {
+//             res.json(response);
+//         }).catch((e) => console.log(e));
+// });
+
+app.get('/bandas', async (req, res) => {
+
+    const query = 'SELECT * FROM bandas';
+    try {
+        const bandas = await sequelize.query(query, {type: sequelize.QueryTypes.SELECT} );
+        res.json(bandas);
+
+    } catch (e) {
+        console.error(e);
+    }
 });
+
 
 
 app.listen(4000, (req, res) => console.log('Escuchando por el 4000'));
